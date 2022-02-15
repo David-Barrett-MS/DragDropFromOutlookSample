212ed202bd898e691cc25f267f8fcd020020eca7
David Barrett <David Barrett@OFFICEDEV>
2022-02-15 14:33:53 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DragDropFromOutlookSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxDropTarget = new System.Windows.Forms.GroupBox();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.groupBoxItemInfo = new System.Windows.Forms.GroupBox();
+            this.textBoxItemInfo = new System.Windows.Forms.TextBox();
+            this.checkBoxSaveItems = new System.Windows.Forms.CheckBox();
+            this.groupBoxItemInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxDropTarget
+            // 
+            this.groupBoxDropTarget.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxDropTarget.Location = new System.Drawing.Point(6, 6);
+            this.groupBoxDropTarget.Name = "groupBoxDropTarget";
+            this.groupBoxDropTarget.Size = new System.Drawing.Size(312, 100);
+            this.groupBoxDropTarget.TabIndex = 0;
+            this.groupBoxDropTarget.TabStop = false;
+            this.groupBoxDropTarget.Text = "Drop Target";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Location = new System.Drawing.Point(324, 12);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // groupBoxItemInfo
+            // 
+            this.groupBoxItemInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxItemInfo.Controls.Add(this.textBoxItemInfo);
+            this.groupBoxItemInfo.Location = new System.Drawing.Point(6, 112);
+            this.groupBoxItemInfo.Name = "groupBoxItemInfo";
+            this.groupBoxItemInfo.Size = new System.Drawing.Size(393, 175);
+            this.groupBoxItemInfo.TabIndex = 2;
+            this.groupBoxItemInfo.TabStop = false;
+            this.groupBoxItemInfo.Text = "Dropped Item(s) Information (shows available formats on drag)";
+            // 
+            // textBoxItemInfo
+            // 
+            this.textBoxItemInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxItemInfo.Location = new System.Drawing.Point(3, 16);
+            this.textBoxItemInfo.Multiline = true;
+            this.textBoxItemInfo.Name = "textBoxItemInfo";
+            this.textBoxItemInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxItemInfo.Size = new System.Drawing.Size(387, 156);
+            this.textBoxItemInfo.TabIndex = 0;
+            // 
+            // checkBoxSaveItems
+            // 
+            this.checkBoxSaveItems.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBoxSaveItems.AutoSize = true;
+            this.checkBoxSaveItems.Enabled = false;
+            this.checkBoxSaveItems.Location = new System.Drawing.Point(324, 89);
+            this.checkBoxSaveItems.Name = "checkBoxSaveItems";
+            this.checkBoxSaveItems.Size = new System.Drawing.Size(78, 17);
+            this.checkBoxSaveItems.TabIndex = 3;
+            this.checkBoxSaveItems.Text = "Save items";
+            this.checkBoxSaveItems.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 291);
+            this.Controls.Add(this.checkBoxSaveItems);
+            this.Controls.Add(this.groupBoxItemInfo);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.groupBoxDropTarget);
+            this.Name = "Form1";
+            this.Text = "Drag/Drop Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxItemInfo.ResumeLayout(false);
+            this.groupBoxItemInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxDropTarget;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.GroupBox groupBoxItemInfo;
+        private System.Windows.Forms.TextBox textBoxItemInfo;
+        private System.Windows.Forms.CheckBox checkBoxSaveItems;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * By David Barrett, Microsoft Ltd. 2022. Use at your own risk.  No warranties are given.
+ * 
+ * DISCLAIMER:
+ * THIS CODE IS SAMPLE CODE. THESE SAMPLES ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND.
+ * MICROSOFT FURTHER DISCLAIMS ALL IMPLIED WARRANTIES INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR OF FITNESS FOR
+ * A PARTICULAR PURPOSE. THE ENTIRE RISK ARISING OUT OF THE USE OR PERFORMANCE OF THE SAMPLES REMAINS WITH YOU. IN NO EVENT SHALL
+ * MICROSOFT OR ITS SUPPLIERS BE LIABLE FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF BUSINESS PROFITS,
+ * BUSINESS INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS) ARISING OUT OF THE USE OF OR INABILITY TO USE THE
+ * SAMPLES, EVEN IF MICROSOFT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. BECAUSE SOME STATES DO NOT ALLOW THE EXCLUSION OR LIMITATION
+ * OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU.
+ * */
+
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace DragDropFromOutlookSample
+{
+    public partial class Form1 : Form
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public sealed class POINTL
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public sealed class SIZEL
+        {
+            public int cx;
+            public int cy;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public sealed class FILEGROUPDESCRIPTORW
+        {
+            public uint cItems;
+            public FILEDESCRIPTORW[] fgd;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public sealed class FILEDESCRIPTORW
+        {
+            public uint dwFlags;
+            public Guid clsid;
+            public SIZEL sizel;
+            public POINTL pointl;
+            public uint dwFileAttributes;
+            public System.Runtime.InteropServices.ComTypes.FILETIME ftCreationTime;
+            public System.Runtime.InteropServices.ComTypes.FILETIME ftLastAccessTime;
+            public System.Runtime.InteropServices.ComTypes.FILETIME ftLastWriteTime;
+            public uint nFileSizeHigh;
+            public uint nFileSizeLow;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string cFileName;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            groupBoxDropTarget.AllowDrop = true;
+            groupBoxDropTarget.DragEnter += GroupBoxDropTarget_DragEnter;
+            groupBoxDropTarget.DragDrop += GroupBoxDropTarget_DragDrop;
+        }
+
+        private void WriteItemToDisk(System.Runtime.InteropServices.ComTypes.IDataObject dataObject, int index)
+        {
+            // Retrieve the given item from the data object and write it to disk
+
+            // Prepare FORMATETC for retrieving data
+            FORMATETC formatetc = new FORMATETC();
+            formatetc.cfFormat = (short)DataFormats.GetFormat("FileContents").Id;
+            formatetc.dwAspect = DVASPECT.DVASPECT_CONTENT;
+            formatetc.lindex = index;
+            formatetc.ptd = new IntPtr(0);
+            formatetc.tymed = TYMED.TYMED_ISTREAM | TYMED.TYMED_ISTORAGE | TYMED.TYMED_HGLOBAL;
+
+            STGMEDIUM medium = new STGMEDIUM();
+            ((System.Runtime.InteropServices.ComTypes.IDataObject)dataObject).GetData(ref formatetc, out medium);
+
+            // How we retrieve the data depends upon the returned store type
+            // This is not implemented as it is currently beyond the scope of this sample
+            // See https://docs.microsoft.com/en-us/windows/win32/com/data-transfer
+        }
+
+        private void GroupBoxDropTarget_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("FileGroupDescriptor"))
+            {
+                IntPtr fileGroupDescriptorWPointer = IntPtr.Zero;
+                try
+                {
+                    // Get FileGroupDescriptorW as a MemoryStream
+                    MemoryStream fileGroupDescriptorStream = (MemoryStream)e.Data.GetData("FileGroupDescriptorW");
+                    byte[] fileGroupDescriptorBytes = new byte[fileGroupDescriptorStream.Length];
+                    fileGroupDescriptorStream.Read(fileGroupDescriptorBytes, 0, fileGroupDescriptorBytes.Length);
+                    fileGroupDescriptorStream.Close();
+
+                    // Copy the file group descriptor into unmanaged memory
+                    fileGroupDescriptorWPointer = Marshal.AllocHGlobal(fileGroupDescriptorBytes.Length);
+                    Marshal.Copy(fileGroupDescriptorBytes, 0, fileGroupDescriptorWPointer, fileGroupDescriptorBytes.Length);
+
+                    // Marshal the unmanaged memory to FILEGROUPDESCRIPTORW struct
+                    object fileGroupDescriptorObject = Marshal.PtrToStructure(fileGroupDescriptorWPointer, typeof(FILEGROUPDESCRIPTORW));
+                    FILEGROUPDESCRIPTORW fileGroupDescriptor = (FILEGROUPDESCRIPTORW)fileGroupDescriptorObject;
+
+                    // Create an array to store file names
+                    string[] fileNames = new string[fileGroupDescriptor.cItems];
+
+                    // Get the pointer to the first file descriptor
+                    IntPtr fileDescriptorPointer = (IntPtr)((int)fileGroupDescriptorWPointer + Marshal.SizeOf(fileGroupDescriptorWPointer));
+
+                    // Loop through to retrieve the list of filenames
+                    for (int fileDescriptorIndex = 0; fileDescriptorIndex < fileGroupDescriptor.cItems; fileDescriptorIndex++)
+                    {
+                        FILEDESCRIPTORW fileDescriptor = (FILEDESCRIPTORW)Marshal.PtrToStructure(fileDescriptorPointer, typeof(FILEDESCRIPTORW));
+                        fileNames[fileDescriptorIndex] = fileDescriptor.cFileName;
+
+                        fileDescriptorPointer = (IntPtr)((int)fileDescriptorPointer + Marshal.SizeOf(fileDescriptor));
+                    }
+
+                    textBoxItemInfo.Text = String.Join(Environment.NewLine, fileNames);
+
+                    if (checkBoxSaveItems.Checked)
+                    {
+                        // Write each of the dropped files to disk
+                        for (int i=0; i<fileNames.Length; i++)
+                            WriteItemToDisk((System.Runtime.InteropServices.ComTypes.IDataObject)e.Data, i);
+                    }
+                    return;
+                }
+                finally
+                {
+                    // Free unmanaged memory pointer
+                    Marshal.FreeHGlobal(fileGroupDescriptorWPointer);
+                }
+            }
+
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                textBoxItemInfo.Text = String.Join(Environment.NewLine, e.Data.GetData(DataFormats.Text, true));
+                return;
+            }
+
+            textBoxItemInfo.Text = "Unsupported drop format";
+        }
+
+        private void GroupBoxDropTarget_DragEnter(object sender, DragEventArgs e)
+        {
+            textBoxItemInfo.Text = String.Join(Environment.NewLine, e.Data.GetFormats(false));
+            if (e.Data.GetDataPresent("FileGroupDescriptorW"))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
